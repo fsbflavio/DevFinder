7137aef784e136a776efb0608b3964ba33d480bd
fsbflavio <flavio.baia@pronetsoftware.com.br>
2020-01-18 20:03:11 -0300

    add web

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -4,39 +4,39 @@ const parseStringAsArray = require('../utils/parseStringAsArray');
 
 module.exports = {
 
-    async index(request, response){
+    async index(request, response) {
         const devs = await Dev.find();
 
         return response.json(devs);
     },
 
     async store(request, response) {
-    const {github_username, techs, latitude, longitude} = request.body;
+        const { github_username, techs, latitude, longitude } = request.body;
 
-    let dev = await Dev.findOne({github_username});
-    if (!Dev){
+        let dev = await Dev.findOne({ github_username });
+        if (!dev) {
 
-        const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
-    
-        const {name = login, avatar_url, bio } = apiResponse.data;
-    
-        const techsArray = parseStringAsArray(techs);
+            const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
 
-        const location = {
-            type: 'Point',
-            coordinates: [longitude, latitude],
+            const { name = login, avatar_url, bio } = apiResponse.data;
+
+            const techsArray = parseStringAsArray(techs);
+
+            const location = {
+                type: 'Point',
+                coordinates: [longitude, latitude],
+            }
+
+            dev = await Dev.create({
+                github_username,
+                name,
+                avatar_url,
+                bio,
+                techs: techsArray,
+                location,
+            });
         }
-    
-        const dev = await Dev.create({
-            github_username,
-            name,
-            avatar_url, 
-            bio,
-            techs: techsArray,
-            location,
-        });
-    }
 
-    return response.json(dev);
+        return response.json(dev);
     }
 };
